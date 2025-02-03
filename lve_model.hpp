deea9de2fb41d4ea447a30438d2c3673ac91e11f
GildedSeraphim <nathanishaaz@gmail.com>
2025-02-03 17:25:18 -0600

    model loading and hashing indices

--- lve_model.hpp
+++ lve_model.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "lve_device.hpp"
+#include <string>
 
 // libs
 #define GLM_FORCE_RADIANS
@@ -8,6 +9,7 @@
 #include <glm/glm.hpp>
 
 // std
+#include <memory>
 #include <vector>
 
 namespace lve {
@@ -16,16 +18,25 @@ public:
   struct Vertex {
     glm::vec3 position{};
     glm::vec3 color{};
+    glm::vec3 normal{};
+    glm::vec2 uv{};
 
     static std::vector<VkVertexInputBindingDescription>
     getBindingDescriptions();
     static std::vector<VkVertexInputAttributeDescription>
     getAttributeDescriptions();
+
+    bool operator==(const Vertex &other) const {
+      return position == other.position && color == other.color &&
+             normal == other.normal && uv == other.uv;
+    }
   };
 
   struct Builder {
     std::vector<Vertex> vertices{};
     std::vector<uint32_t> indices{};
+
+    void loadModel(const std::string &filepath);
   };
 
   LveModel(LveDevice &device, const LveModel::Builder &builder);
@@ -33,6 +44,9 @@ public:
 
   LveModel(const LveModel &) = delete;
   LveModel &operator=(const LveModel &) = delete;
+
+  static std::unique_ptr<LveModel>
+  createModelFromFile(LveDevice &device, const std::string &filepath);
 
   void bind(VkCommandBuffer commandBuffer);
   void draw(VkCommandBuffer commandBuffer);
